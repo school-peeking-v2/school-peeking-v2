3a19318cc52bada97fa9439b9e41f2a4a6df7ebe
ovoHJ <s2018w01@gmail.com>
2020-05-16 16:09:07 +0900

    leader, rank

---
+++ rank.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" shrink-to-fit="no">
+
+    <title>첫 등교는 IP로!</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/faviconSchool.ico" />
+    <link rel="icon" href="images/faviconSchool.ico" />
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+
+    <!-- Font -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css">
+
+    <link rel="stylesheet" href="./css/grayscale.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/rank.css" />
+  </head>
+  <body>
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-expand-lg navbar-light fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand js-scroll-trigger" href="index.html"
+          >첫 등교는 IP로!</a
+        >
+        <button
+          class="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto list-unstyled">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#" id="roadview"
+                >로드뷰</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link js-scroll-trigger"
+                href="./inventory.html"
+                id="inventory"
+                >인벤토리</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="./coupon.html" id="coupon"
+                >인증서</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="./rank.html" id="ranking"
+                >랭킹</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#" id="logout"
+                >로그아웃</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="invent-section text-center">
+        <div class="inven">
+            <h1 class="inven-title">정답자 명단</h1>
+        </div>
+        <div class="container">
+            <div class="col-sm">
+                <p class="inven-inst" id="inven-inst">퀴즈의 정답을 맞춘 사람을 확인하세요!</p>
+            </div>
+
+            <div class="inven-group">
+                <span class="inven-rank">1</span>
+                <div class="inven-me-title">
+                    <span>배주현</span>|<span>10000점</span>
+                </div>
+            </div>
+            <div class="inven-group">
+                <span class="inven-rank">2</span>
+                <div class="inven-me-title">
+                    <span>배주현</span>|<span>10000점</span>
+                </div>
+            </div>
+            <div class="inven-group">
+                <span class="inven-rank">3</span>
+                <div class="inven-me-title">
+                    <span>배주현</span>|<span>10000점</span>
+                </div>
+            </div>
+            <div class="inven-group">
+                <span class="inven-rank">4</span>
+                <div class="inven-me-title">
+                    <span>배주현</span>|<span>10000점</span>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+    <script src="./js/jquery.min.js"></script>
+    <script src="js/rank.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="./js/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="./js/grayscale.min.js"></script>
+  </body>
+</html>
