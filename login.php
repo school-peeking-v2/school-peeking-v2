254f33dbf11218236b1768ebe20ec96a03e8eb50
ovoHJ <s2018w01@gmail.com>
2020-05-27 11:27:43 +0900

    update login, signup

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    
+
     if(isset($_SESSION['ss_mb_id'])) {
         echo "<script>location.href = './index.php';</script>";
     }
@@ -31,43 +31,48 @@
       href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css"
     />
 
-    <link rel="stylesheet" href="./css/grayscale.css" />
     <link rel="stylesheet" href="./css/style.css" />
     <link rel="stylesheet" href="./css/login.css" />
   </head>
   <body>
-    <img id="logo" src="./images/mirim.PNG" />
-    <p class="title"><b>미림여자정보과학고등학교</b></p>
-    <img id="char" src="./images/char_icon.png" />
-    <p>
-      <form action="src/login_back.php" onsubmit="return login_submit(this);" method="post">
-        <input type="text" name="email" id="email" placeholder="이메일" />
-        <input type="password" name="pwd" id="pwd" placeholder="비밀번호" />
-        <button type="submit" id="btn_login">로그인</button>
-      </form>
-    </p>
 
-    <p class="signup">
-      아직 첫등교로는 IP로의 회원이 아니신가요 ? <br />
-      <b><a href="./signup.php">지금 가입하러 가기</a></b>
-    </p>
+    <div class="view">
+        <div class="logo">
+            <img src="images/mirim.PNG" />
+            <span class="school-name">미림여자정보과학고등학교</span>
+        </div>
+        <div class="profile">
+            <img class="person" src="images/person.png" />
+        </div>
+        <div class="login">
+            <form action="src/login_back.php" onsubmit="return login_submit(this);" method="post">
+                <input type="email" name="email" id="email" placeholder="이메일" /><br/>
+                <input type="password" placeholder="비밀번호"><br/>
+                <button type="submit" id="btn_login">로그인</button>
+            </form>
+        </div>
+        <div class="join">
+            아직 첫등교는 IP로의 회원이 아니신가요?<br/>
+            <a href="./signup.php" class="click_join"><b>지금 가입하러 가기</b></a>
+        </div>
+    </div>
+
+    <script>
+        function login_submit(f) {
+            if(!f.email.value) {
+                alert("이메일을 입력하세요");
+                f.email.focus();
+                return false;
+            }
+
+            if(f.pwd.value.length < 4) {
+                if(!f.pwd.value) alert("비밀번호를 입력하세요");
+                else alert("비밀번호는 4글자 이상입니다. 다시 입력하세요.");
+                f.pwd.value = null;
+                f.pwd.focus();
+                return false;
+            }
+        }
+    </script>
   </body>
 </html>
-
-<script>
-    function login_submit(f) {
-        if(!f.email.value) {
-            alert("이메일을 입력하세요");
-            f.email.focus();
-            return false;
-        }
-
-        if(f.pwd.value.length < 4) {
-            if(!f.pwd.value) alert("비밀번호를 입력하세요");
-            else alert("비밀번호는 4글자 이상입니다. 다시 입력하세요.");
-            f.pwd.value = null;
-            f.pwd.focus();
-            return false;
-        }
-    }
-</script>
